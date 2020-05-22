a1409f596680361ac2c9751869961e8e01936e9e
stephen-tomaszewski <stephen.tomaszewski17@gmail.com>
2020-05-22 14:11:41 -0700

    functional app

--- manage.py
+++ manage.py
@@ -1,8 +1,20 @@
+import sys
+
 from flask.cli import FlaskGroup
-from project import app
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
-cli = FlaskGroup(app)
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     cli()
--- project/__init__.py
+++ project/__init__.py
@@ -1,18 +1,31 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-api = Api(app)
-
-app.config.from_object('project.config.DevelopmentConfig')
-
-
-class Ping(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+#instantiate the db
+db = SQLAlchemy()
 
 
-api.add_resource(Ping, '/ping')
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    #register blueprints
+    from project.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return{'app': app, 'db': db}
+
+    return app
